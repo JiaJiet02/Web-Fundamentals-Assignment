425f21b38818edd535a8bf2c215e97ea66c742fa
JiaJiet02 <137740225+JiaJiet02@users.noreply.github.com>
2023-12-03 23:34:23 +0800

    Day 1 end

--- Coffee/Public/JS/mobilescript.js
+++ Coffee/Public/JS/mobilescript.js
@@ -4,3 +4,25 @@ const navlinks = document.querySelector('.nav-links')
 menuBtn.addEventListener('click', ()=>{
     navlinks.classList.toggle('mobile-menu')
 })
+
+function register() {
+    var email= document.getElementById("email").value;
+    var password= document.getElementById("password").value;
+
+    alert("Registered successfully!");
+}
+
+function login() {
+    var email= document.getElementById("email").value;
+    var password= document.getElementById("password").value;
+
+    alert("Login successful!");
+}
+
+function message() {
+    var name= document.getElementById("name").value;
+    var email= document.getElementById("email").value;
+    var feedback = document.getElementById("feedback").value;
+
+    alert("Submitted successfully!");
+}
